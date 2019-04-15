966d94e2fe081e8718559a0f9c72e136a7593ae7
Yusuf Bahadır Şahan <33758240+ybsahan@users.noreply.github.com>
2019-04-15 10:51:01 +0300

    Update sumfiles.sh

--- sumfiles.sh
+++ sumfiles.sh
@@ -1,41 +1,37 @@
 #!/bin/bash
 
-j=0
-while [ $j -lt 12 ]
-do
-	sum1=0
-	sum2=0
-	sum3=0
-	sum4=0
-	for i in 1 2 3
-	do
-		a=$(cat osman$i.txt)
-		((sum1 += $a))
-	
-	done
-	((j++))
-	
-	for i in 4 5 6
-	do
-		a=$(cat osman$i.txt)
-		((sum2 += $a))
-	done
-	
-	for i in 7 8 9
-	do
-		a=$(cat osman$i.txt)
-		((sum3 += $a))
-	done
-	
-	for i in 10 11 12
-	do
-		a=$(cat osman$i.txt)
-		((sum4 += $a))
-	done
-	
-done
-echo "3, $sum1"
-echo "3, $sum2"
-echo "3, $sum3"
-echo "3, $sum4"
+filecount=$(find . -type f -name "osman*.txt" | wc -l)
+echo $filecount
 
+forcount=$(($filecount / 3))
+echo $forcount
+sum=0
+i=1
+while [ $i -le $filecount ]
+do
+	sum=0
+
+	a=$(cat osman$i.txt)
+	((i++))
+	if [ -f osman$i.txt ]
+	then
+	b=$(cat osman$i.txt)
+	else
+	b=0
+	fi
+	((i++))
+	if [ -f osman$i.txt ]
+	then
+	c=$(cat osman$i.txt)
+	else
+	c=0
+	fi
+	((i++))
+	echo $a $b $c
+	sum=$(expr $a + $b + $c)
+
+	
+	
+	
+	echo 3, $sum
+done
